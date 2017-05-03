d8edc8706176379aed501bf1060d6088f845a3c7
schneppd <david.schnepp@schneppd.com>
2017-05-03 09:50:57 +0200

    switch to auto layout's logic

--- Composition03/Composition03/ViewController.swift
+++ Composition03/Composition03/ViewController.swift
@@ -24,49 +24,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //ensure margins when view rotates
-    override func viewWillLayoutSubviews() {
-        
-        if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft || UIDevice.current.orientation == UIDeviceOrientation.landscapeRight {
-            
-            var rect = TopLeftView.frame
-            rect.size.width = 254
-            rect.size.height = 130
-            TopLeftView.frame = rect
-            
-            rect = TopRightView.frame
-            rect.origin.x = 294
-            rect.size.width = 254
-            rect.size.height = 130
-            TopRightView.frame = rect
-            
-            rect = BottomView.frame
-            rect.origin.y = 170
-            rect.size.width = 528
-            rect.size.height = 130
-            BottomView.frame = rect
-            
-        } else {
-            
-            var rect = TopLeftView.frame
-            rect.size.width = 130
-            rect.size.height = 254
-            TopLeftView.frame = rect
-            
-            rect = TopRightView.frame
-            rect.origin.x = 170
-            rect.size.width = 130
-            rect.size.height = 254
-            TopRightView.frame = rect
-            
-            rect = BottomView.frame
-            rect.origin.y = 295
-            rect.size.width = 280
-            rect.size.height = 254
-            BottomView.frame = rect
-        }
-    }
-
 
 }
 
