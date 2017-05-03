14a477d0361be911200a00da449318b15e224cdb
schneppd <david.schnepp@schneppd.com>
2017-05-03 09:28:44 +0200

    add logic to manage view's position

--- Composition03/Composition03/ViewController.swift
+++ Composition03/Composition03/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TopLeftView: UIScrollView!
+    @IBOutlet weak var TopRightView: UIScrollView!
+    @IBOutlet weak var BottomView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,49 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //ensure margins when view rotates
+    override func viewWillLayoutSubviews() {
+        
+        if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft || UIDevice.current.orientation == UIDeviceOrientation.landscapeRight {
+            
+            var rect = TopLeftView.frame
+            rect.size.width = 254
+            rect.size.height = 130
+            TopLeftView.frame = rect
+            
+            rect = TopRightView.frame
+            rect.origin.x = 294
+            rect.size.width = 254
+            rect.size.height = 130
+            TopRightView.frame = rect
+            
+            rect = BottomView.frame
+            rect.origin.y = 170
+            rect.size.width = 528
+            rect.size.height = 130
+            BottomView.frame = rect
+            
+        } else {
+            
+            var rect = TopLeftView.frame
+            rect.size.width = 130
+            rect.size.height = 254
+            TopLeftView.frame = rect
+            
+            rect = TopRightView.frame
+            rect.origin.x = 170
+            rect.size.width = 130
+            rect.size.height = 254
+            TopRightView.frame = rect
+            
+            rect = BottomView.frame
+            rect.origin.y = 295
+            rect.size.width = 280
+            rect.size.height = 254
+            BottomView.frame = rect
+        }
     }
 
 
